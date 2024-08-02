25196e7abf7ea461f4bc3c08472a9e6e5725aeab
---- <x@gmail.com>
2024-08-01 19:44:20 -0500

    VERSION 1.0

---
+++ DividirDosNumeros/src/main/java/com/mycompany/dividirdosnumeros/DividirDosNumeros.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.dividirdosnumeros;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class DividirDosNumeros {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
